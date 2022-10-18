b3f8c9d4361cdbf879d55442af2f402cf00a60b1
Marcos <jmarcoshenrich@gmail.com>
2022-10-18 13:32:48 -0700

    begin of memory

---
+++ Memory Puzzle/card.rb
@@ -0,0 +1,41 @@
+require_relative "board"
+require_relative "game"
+
+class Card
+    attr_reader :value
+    @@all_cards = []
+
+    def self.all_cards
+        @@all_cards
+    end
+
+    def initialize(value)
+        @value = value
+        @face_up = false
+        @@all_cards << self
+    end
+
+    def status
+        return @value if @face_up == true
+        nil
+    end
+
+    def hide
+        @face_up = false
+    end
+
+    def reveal
+        @face_up = true
+    end
+
+    def to_s
+       nil
+       # we may need this, purpose unclear
+    end
+
+    def ==(other_card)
+        return true if self.value == other_card.value
+        false
+    end
+
+end
